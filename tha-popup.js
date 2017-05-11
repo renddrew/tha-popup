5bf18d8dc90cc866dab541e351bfe1897c161c9e
renddrew <rend.drew@gmail.com>
2017-05-11 11:59:51 -0400

    convert to ajax loading

--- tha-popup.js
+++ tha-popup.js
@@ -1,13 +1,77 @@
 jQuery(document).ready(function($){
-	$('body').on('click', '[data-tha-popup-id]', function(){
-		var id = $(this).data('tha-popup-id');
-		var target = $('.tha-popup-preload[data-tha-popup-target="' + id + '"]');
 
-		$(target).fadeIn();
-		return false;
+
+	function tha_get_popup_content(id=null, show=1, bg=null, color=null){
+
+		var anchored = $('body .tha-popup-anchor[data-tha-popup-id="'+id+'"]');
+
+		if(anchored.length){
+			$(anchored).find('.tha-popup-container, .tha-pop-shadow').fadeIn(function(){
+				console.log('showed anchored ' + $(anchored).data('tha-popup-id'));
+			});
+			return;
+		} 
+
+		if(id){
+			var data = { 'action':'tha_ajax', 'id':id };
+			$.ajax({
+				 url:tha_popup.thaAjaxUrl,
+				 type:'POST',
+				 data: data,
+				 success: function(data){
+				 	var data = JSON.parse(data);
+				 	var styles= '';
+
+				 	if(bg){
+				 		styles+='background-color:'+bg+';';
+				 	}
+
+				 	if(color){
+				 		styles+='color:'+color+';';
+				 	}
+
+				 	var out = '<div class="tha-popup-anchor" data-tha-popup-id="'+id+'">' + 
+			                   	  '<div class="tha-pop-shadow"></div>' + 
+				                  '<div class="tha-popup-container">' +
+				                  	  '<div class="tha-popup-hide">x</div>' +
+				                      '<div class="tha-popup-body" style="'+styles+'">' +
+				                         data.content +
+			                           '</div>' +
+			                        '</div>' +
+			                     '</div>';
+			        $('body').append(out);  
+			        console.log('anchored: ' + id);
+
+			        if(show){
+				        $('.tha-popup-anchor[data-tha-popup-id="'+id+'"]').find('.tha-popup-container, .tha-pop-shadow').fadeIn(100);
+				    }
+				 }
+			});
+		}
+	}
+
+
+	$(window).load(function(){
+		$('.tha-popup-trigger[data-tha-popup-lazy="true"]').each(function(i,v){
+			var id = $(v).data('tha-popup-id');
+			var bg = $(v).data('tha-popup-bg');
+			var color = $(v).data('tha-popup-color');
+			tha_get_popup_content(id, 0, bg, color);
+		});
+	});
+
+	$('body').on('click', '.tha-popup-trigger[data-tha-popup-id]', function(e){
+		e.preventDefault();
+		var id = $(this).data('tha-popup-id');
+		var bg = $(this).data('tha-popup-bg');
+		var color = $(this).data('tha-popup-color');
+		tha_get_popup_content(id, 1, bg, color);
 	});
 
 	$('body').on('click', '.tha-popup-hide', function(){
-		$('.tha-popup-preload').fadeOut();
+		$('.tha-popup-container, .tha-pop-shadow').fadeOut(100);
 	});
+
+
 });
+
--- tha-popup.php
+++ tha-popup.php
@@ -15,36 +15,71 @@ add_action( 'wp_enqueue_scripts', 'tha_enqueues');
 function tha_enqueues(){
   wp_enqueue_style( 'tha-popup-css', plugin_dir_url( __FILE__ ) . 'tha-popup.css');
   wp_enqueue_script( 'tha-popup-js', plugin_dir_url( __FILE__ ) . 'tha-popup.js', array('jquery'));
+  wp_localize_script( 'tha-popup-js', 'tha_popup', array(
+    'thaAjaxUrl'=> admin_url('admin-ajax.php')
+    ));
 }
 
 
-add_shortcode('tha_popup', 'tha_popup');
-function tha_popup($atts){
+
+
+
+// shortcode function to add popup link 
+function tha_popup_link($atts){
     extract(shortcode_atts(array(
-      'pgid' => '',
-      'bg' => '#fff',
-      'color' => '#000'
+      'page_id' => '',
+      'link_text' => '',
+      'classes' => '',
+      'lazy_load' => 'true',
+      'bg' => '',
+      'color' => ''
     ), $atts));
 
-    $page_data = get_post($pgid);
-
-    if($page_data) {
-
-       $content= apply_filters('the_content', $page_data->post_content);
-
-       $out = '<div class="tha-popup-preload" data-tha-popup-target="' . $pgid . '">
-       <div class="tha-pop-shadow"></div>
-       <div class="tha-popup-container">
-        <div class="tha-popup-body" style="background-color:' . $bg . ';color:' . $color . '">' . $content . '</div><div class="tha-popup-hide">x</div></div>
-       </div>';
-       return $out;
+    if($lazy_load === 'true'){
+      $data.= ' data-tha-popup-lazy="true"';
+    }elseif($lazy_load === 'onclick'){
+      $data.= ' data-tha-popup-lazy="onclick"';
     }
 
+    if($bg){
+      $data.= ' data-tha-popup-bg="' . $bg . '"';
+    }
+
+    if($color){
+      $data.= ' data-tha-popup-color="' . $color . '"';
+    }
+
+    if($page_id){
+      $out = '<a href="#" data-tha-popup-id="' . $page_id . '" class="tha-popup-trigger ' . $classes . '" ' . $data . '>' . $link_text . '</a>';
+      return apply_filters('the_content', $out);
+    }
+}
+add_shortcode('tha_popup_link', 'tha_popup_link');
+
+
+
+
+function tha_popup_content($pgid, $bg='#fff', $color='#000'){
+   $page_data = get_post($pgid);
+   if($page_data){
+       $content= apply_filters('the_content', $page_data->post_content);
+       return $content;
+    }
 }
 
 
+function tha_ajax(){
+  if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+    $id = $_POST['id'];
+    $data['id'] = $id;
+    $data['content'] =  tha_popup_content($id); 
+    echo json_encode($data);
+  }
+  die();
+}
 
-
+add_action('wp_ajax_tha_ajax','tha_ajax');
+add_action('wp_ajax_nopriv_tha_ajax','tha_ajax');
 
 
 
