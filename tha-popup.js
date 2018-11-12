3d6b48878c4cfc19b49ca8b61314c85490903583
Andrew Rendall <rend.drew@gmail.com>
2018-11-12 09:18:11 -0500

    functionality to trigger on exit intent - untested

--- tha-popup.js
+++ tha-popup.js
@@ -10,4 +10,97 @@ jQuery(document).ready(function($){
 	$('body').on('click', '.tha-popup-hide', function(){
 		$('.tha-popup-preload').fadeOut();
 	});
+
+
+
+	// Exit Intent Trigger Preloaded Popups
+    tcPop = {
+        exitDelay:15000,
+        loadTime: '',
+
+        getDomain: function(){
+            var domain = '';
+            if(tapclicks.siteUrl.indexOf('tapclicks.com')!== -1){
+                domain = 'tapclicks.com';
+            }
+            return domain;
+        },
+
+        getLoadTime: function(){
+            if(!this.loadTime){
+                this.loadTime = new Date().getTime();
+            }
+            return this.loadTime;
+        },
+
+        checkPopupCookie: function(id){
+            return Cookies.get('tcPopView_' + id);
+        },
+
+        setPopupCookie: function(id, expire){
+            var expire = expire || 3;
+            var domain = this.getDomain();
+            Cookies.set('tcPopView_' + id, '1', { path: '/', expires:expire, domain:domain });
+        },
+        
+        triggerExitPopups: function(){
+            var self = this;
+            $('.preloaded-page-popup[data-exit-trigger]').each(function(){
+                var id = $(this).data('popup-id');
+                var exitDelay = $(this).data('exit-delay');
+                var expire = $(this).data('exit-intent-expire');
+                if(!exitDelay){
+                    exitDelay = self.exitDelay;
+                }
+                if(!self.checkPopupCookie(id)){
+                    var date = new Date;
+                    var time = date.getTime();
+                    if(time > (self.getLoadTime() + exitDelay)){
+                        tc_preloaded_popup(id);
+                        self.setPopupCookie(id, expire);
+                    }
+                }
+            });
+        },
+
+        watchMouse: function(){
+            this.getLoadTime;
+            $(document).mousemove(function(e){
+                /*
+                Notes
+                Side thought, could add in functionality for checking whether it's windows or mac or linux and which browser so we could determine if they are moving to close the browser for the pop up.
+                */
+                var  
+                yOff = window.pageYOffset,
+                xOff = window.pageXOffset,
+                pageX = e.pageX,
+                pageY = e.pageY,
+                headerSp=jQuery("#header-space"),
+                navHH=Math.floor(headerSp.height()*(1/4));
+                
+                if(((pageY - yOff)<=navHH) || ((pageX - yOff)<=20)){
+                   tcPop.triggerExitPopups();  
+                }
+            });
+        }
+    }
+
+
+    if( $('.preloaded-page-popup[data-exit-trigger]').length ){
+        tcPop.watchMouse();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
--- tha-popup.php
+++ tha-popup.php
@@ -24,15 +24,34 @@ function tha_popup($atts){
       'pgid' => '',
       'bg' => '#fff',
       'color' => '#000'
+      'exit_intent' => '',
+      'exit_delay' => '',
+      'exit_intent_expire' => '',
     ), $atts));
 
     $page_data = get_post($pgid);
 
     if($page_data) {
 
+        $is_exit_attr = '';
+        if($exit_intent){
+            $is_exit_attr = ' data-exit-trigger="1" ';
+        }
+
+        $exit_delay_attr = '';
+        if($exit_delay){
+            $exit_delay_attr = ' data-exit-delay="' . $exit_delay . '" ';
+        }
+
+        $exit_cookie_expire_attr = '';
+        if($exit_intent_expire){
+            $exit_cookie_expire_attr = ' data-exit-intent-expire="' . $exit_intent_expire . '" ';
+        }
+
+
        $content= apply_filters('the_content', $page_data->post_content);
 
-       $out = '<div class="tha-popup-preload" data-tha-popup-target="' . $pgid . '">
+       $out = '<div class="tha-popup-preload" data-tha-popup-target="' . $pgid . '" ' . $is_exit_attr . $exit_delay_attr . $exit_cookie_expire_attr .'>
        <div class="tha-pop-shadow"></div>
        <div class="tha-popup-container">
         <div class="tha-popup-body" style="background-color:' . $bg . ';color:' . $color . '">' . $content . '</div><div class="tha-popup-hide">x</div></div>
